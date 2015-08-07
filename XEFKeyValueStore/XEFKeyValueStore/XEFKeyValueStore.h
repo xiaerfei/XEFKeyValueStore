2ec942576be08ba8bdb78617fbd731331ce3846b
xiaerfei <xiaerfei1989@163.com>
2015-08-07 16:36:46 +0800

    add fmdb

---
+++ XEFKeyValueStore/XEFKeyValueStore/XEFKeyValueStore.h
@@ -0,0 +1,13 @@
+//
+//  XEFKeyValueStore.h
+//  XEFKeyValueStore
+//
+//  Created by xiaerfei on 15/8/7.
+//  Copyright (c) 2015年 RongYu100. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XEFKeyValueStore : NSObject
+
+@end
---
+++ XEFKeyValueStore/XEFKeyValueStore/XEFKeyValueStore.m
@@ -0,0 +1,13 @@
+//
+//  XEFKeyValueStore.m
+//  XEFKeyValueStore
+//
+//  Created by xiaerfei on 15/8/7.
+//  Copyright (c) 2015年 RongYu100. All rights reserved.
+//
+
+#import "XEFKeyValueStore.h"
+
+@implementation XEFKeyValueStore
+
+@end
